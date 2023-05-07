c2e66aad237b90e374832d42c7b647aa018099c1
sion@mrmarvis.com <sion@mrmarvis.com>
2023-05-07 20:48:19 +0200

    count2

---
+++ main/js/wordCount2.js
@@ -0,0 +1,21 @@
+
+/* This script keeps track of the amount of words in the text area and displays it in a counter*/
+
+function wordCounter2(str) {
+    if (!str || typeof str !== 'string') {
+      return 0;
+    }
+
+    const words = str.trim().split(/\s+/);
+    return words.length;
+  }
+
+function countWords2() {
+    const textInput2 = document.getElementById("textInput2");
+    const wordCountElement = document.getElementById("wordCount2");
+    const wordCount = wordCounter2(textInput2.value);
+    wordCountElement.textContent = wordCount;
+  }
+
+const textInput2 = document.getElementById("textInput2");
+textInput2.addEventListener("input", countWords2);
