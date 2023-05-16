2859fa54393f51b7807207b4285494cc4c7446e5
sion@mrmarvis.com <sion@mrmarvis.com>
2023-05-16 18:00:28 +0200

    installed cors app again

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ dotenv.config();
 import { Configuration, OpenAIApi } from "openai";
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const configuration = new Configuration({
     organization: "org-YH2aAHYnQZH20vqTPLBIlRiV",
@@ -20,6 +21,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post("/", async (req, res) => {
 
