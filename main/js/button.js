4daa25796d6c0b4b1d423a6e17d305730dd1c0ae
sion@mrmarvis.com <sion@mrmarvis.com>
2023-05-18 10:02:57 +0200

    thank you page

--- main/js/button.js
+++ main/js/button.js
@@ -1,7 +1,8 @@
 
 /* This script makes it possible to hop to another section within the same page */
 
-function goToPage(pageId) {
+function goToPage(event, pageId) {
+    event.preventDefault();
     var pages = document.getElementsByTagName('section');
     for (var i = 0; i < pages.length; i++) {
         pages[i].style.display = 'none';
