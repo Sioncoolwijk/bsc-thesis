6e687fe78cb3b6327e30c14d3069c715da3d3658
sion@mrmarvis.com <sion@mrmarvis.com>
2023-05-20 14:02:03 +0200

    all

---
+++ main/connect.php
@@ -0,0 +1,16 @@
+<?php
+    $used_gpt_user_answer = $_POST['answer'];
+
+    $conn = new mysqli('xxxx','xxxx', 'xxxx!','xxxx');
+    if ($conn->connect_error){
+        die('Connection Failed: '.$conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO experiment_info (used_gpt_user_answer) VALUES (?)");
+        $stmt->bind_param("s", $used_gpt_user_answer); 
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        header('Location: thankyou');
+        exit();
+    }
+?>
