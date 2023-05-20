1608ef1bfa585bf7674c0cef1592423d95ef0932
sion@mrmarvis.com <sion@mrmarvis.com>
2023-05-20 13:57:45 +0200

    added db hashed db info

--- main/js/button.js
+++ main/js/button.js
@@ -1,7 +1,7 @@
 /* This script makes it possible to hop to another section within the same page */
 
 function goToPage(event, pageId) {
-    event.preventDefault();
+
     var pages = document.getElementsByTagName('section');
     for (var i = 0; i < pages.length; i++) {
         pages[i].style.display = 'none';
